06fdab6130c78fa03065c104e588d4a3fa5068dd
at9418 <at672@drexel.edu>
2015-08-17 07:40:01 -0400

    Create sendtextusingtwilio.py
    Send text to a mobile device using Twilio API! 
    Disclaimer: The code is copied from the Twilio website for my learning purpose.

---
+++ twilio-project/sendtextusingtwilio.py
@@ -0,0 +1,12 @@
+from twilio.rest import TwilioRestClient
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.sms.messages.create(
+    body="Hello!!",
+    to="+XXXXXXXX",    # Replace with your phone number
+    from_="+XXXXXXXX") # Replace with your Twilio number
+print (message.sid)
