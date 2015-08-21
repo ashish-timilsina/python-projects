46814974f6a0f03b92b562d52b715681245dc295
at9418 <at672@drexel.edu>
2015-08-21 08:30:30 -0400

    Update dice_roll.py

--- DiceRoll-Blackjack/dice_roll.py
+++ DiceRoll-Blackjack/dice_roll.py
@@ -3,17 +3,37 @@ __author__ = 'Ashish'
 import random
 
 
-def dice_roll():
-    number = 0
-    while number <21:
-        roll = int(random.randint(1,6))
-        print("dice roll: "+str(roll))
-        number = number + roll
 
-    if number == 21:
-        print("you've won with total of: "+str(number))
+#dice roll that gets a random number between 1 & 6 inclusive
+def dice_roll():
+    return random.randint(1,6)
+
+#two players roll the dice to reach the goal of 21
+def dice_game():
+    playerOneTotal = 0
+    playerTwoTotal = 0
+
+    while (playerOneTotal & playerTwoTotal) <21:
+        roll1 = dice_roll()
+        roll2 = dice_roll()
+        playerOneTotal = playerOneTotal + roll1
+        playerTwoTotal = playerTwoTotal + roll2
+
+
+    if playerOneTotal == 21:
+        print("Player One got 21. Player One WINS!")
+
+    elif playerTwoTotal == 21:
+        print("Player Two got 21. Player Two WINS!")
+
+    elif (playerOneTotal & playerTwoTotal) == 21:
+        print("draw")
+
     else:
-        print("you've exceeded total of 21. Total is: "+str(number) )
+        print("Both players have exceeded total of 21. Player 1 got %d and player 2 got %d" % (playerOneTotal, playerTwoTotal))
+
         exit()
 
-dice_roll()
+dice_game()
+
+
